58a098aee09e637a1b5f3773e285e9cc8e54f72e
Murilo Henrique Lodi <murlxff@gmail.com>
2025-09-22 15:44:26 -0300

    feat/navbar

--- includes/head.php
+++ includes/head.php
@@ -1,8 +1,20 @@
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-    <script src="assets/js/functions.js"></script>
-    <link rel="stylesheet" href="assets/css/style.css">
-    <link rel="stylesheet" href="assets/css/tailwind.css">
-</head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+<title>Document</title>
+
+<!-- Opcional: melhora performance -->
+<link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+
+<!-- Escolha a família e pesos desejados -->
+<link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600&display=swap" rel="stylesheet">
+
+
+<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css">
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js"></script>
+<link rel="stylesheet" href="assets/css/style.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/7.0.1/css/all.min.css">
+
+
+<script src="assets/js/functions.js" defer></script>
--- index.php
+++ index.php
@@ -1,7 +1,34 @@
-<!DOCTYPE html>
-<html lang="en">
-    <? include 'includes/head.php' ?>
-<body>
-    
+<!-- index.php -->
+<!doctype html>
+<html lang="pt-br">
+
+<head>
+  <?php include 'includes/head.php' ?>
+</head>
+
+<body class="">
+  <section class="navbar">
+    <div class="container">
+      <div class="list-unstyled">
+        <a href=""><img class="logo rounded-5" src="assets/img/logo.jpeg" alt=""></a>
+      </div>
+
+      <div>
+        <ul class="d-flex list-unstyled">
+          <li class=""><a href="">Página inicial</a></li>
+          <li><a href="">Loja</a></li>
+          <li><a href="">Termos</a></li>
+        </ul>
+      </div>
+
+      <div>
+        <ul class="d-flex list-unstyled">
+          <li><a href=""><i class="fa-solid fa-circle-user"></i><span>Fazer login</span></a></li>
+          <li><a href=""><i class="fa-solid fa-cart-shopping"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </section>
 </body>
+
 </html>
